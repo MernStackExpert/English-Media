74a55ca827c8ba4327d3bb8ed295af75a9404d3b
MernStackExpert <mdnirob30k@gmail.com>
2025-09-03 22:52:10 +0600

    Secend Commit

--- script/script.js
+++ script/script.js
@@ -5,19 +5,32 @@ const loadLessions = () => {
 };
 
 
-const loadLevelWord = (id) => {
-
-  const url = `https://openapi.programming-hero.com/api/level/${id}`
-  fetch(url).then(res => res.json()).then(data => displayLevelWOrd(data.data))
+const removeActive = () => {
+  const lessonButtons = document.querySelectorAll(".lesson-btn");
+  lessonButtons.forEach(btn => btn.classList.remove("active"))
 }
 
+
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+
+      removeActive();
+      const clickBtn = document.getElementById(`lesson-btn-${id}`);
+      clickBtn.classList.add("active");
+
+      displayLevelWOrd(data.data)
+    });
+
+};
 
 const displayLevelWOrd = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
 
-
-  if(words.length == 0){
+  if (words.length == 0) {
     wordContainer.innerHTML = `
     <div class="text-center col-span-full rounded-xl py-10 space-y-6">
         <img class="mx-auto" src="./assets/alert-error.png"/>
@@ -27,14 +40,19 @@ const displayLevelWOrd = (words) => {
     return;
   }
 
-
-  words.forEach(word => {
+  words.forEach((word) => {
     const card = document.createElement("div");
     card.innerHTML = `
     <div class="bg-white rounded-xl text-center shadow-sm py-10 px-5 space-y-4">
-        <h2 class="font-bold text-2xl ">${word.word ? word.word : "শব্দ পাওয়া যাইনি"}</h2>
+        <h2 class="font-bold text-2xl ">${
+          word.word ? word.word : "শব্দ পাওয়া যাইনি"
+        }</h2>
         <p class="font-semibold">Meaning /Pronounciation</p>
-        <p class="font-bangla text-2xl font-semibold">${word.meaning ? word.meaning : "অর্থ পাওয়া যাইনি"} / ${word.pronunciation ? word.pronunciation : "Pronunciation পাওয়া যাইনি"}</p>
+        <p class="font-bangla text-2xl font-semibold">${
+          word.meaning ? word.meaning : "অর্থ পাওয়া যাইনি"
+        } / ${
+      word.pronunciation ? word.pronunciation : "Pronunciation পাওয়া যাইনি"
+    }</p>
 
         <div class="flex justify-between items-center">
           <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
@@ -43,22 +61,21 @@ const displayLevelWOrd = (words) => {
       </div>
     `;
 
-     wordContainer.append(card)
-
+    wordContainer.append(card);
   });
-}
+};
 
 const displayLesson = (lessons) => {
   const levelContainer = document.getElementById("level-container");
   levelContainer.innerHTML = "";
-  for(let lesson of lessons) {
+  for (let lesson of lessons) {
     const btnDiv = document.createElement("div");
 
     btnDiv.innerHTML = `
-     <button onclick = loadLevelWord(${lesson.level_no}) class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i> lesson - ${lesson.level_no}</button>
+     <button id="lesson-btn-${lesson.level_no}" onclick = loadLevelWord(${lesson.level_no}) class="btn btn-outline btn-primary lesson-btn"><i class="fa-solid fa-book-open"></i> lesson - ${lesson.level_no}</button>
     `;
 
-  levelContainer.append(btnDiv);
+    levelContainer.append(btnDiv);
   }
 };
 
